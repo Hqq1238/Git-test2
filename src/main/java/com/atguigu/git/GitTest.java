bf159e96c7983e8daea4dca23c6def87b156c951
hqq <1756032619@qq.com>
2022-01-13 11:48:13 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName GitTest
+ * @Description
+ * @Author HQQ
+ * @Date 2022/1/13 11:45
+ * @Version 1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
